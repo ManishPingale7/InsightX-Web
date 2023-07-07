d853e1988b0c7b3fa99aaee6df8d17edd4412a65
Manish Pingale <manishpingale2005@gmail.com>
2023-07-07 15:31:57 +0530

    Model integration done!

--- PredictiX/web/views.py
+++ PredictiX/web/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import joblib
 
 # Create your views here.
+
+def home(request):
+    print(makePrediction())
+    return HttpResponse(makePrediction())
+
+
+def makePrediction():
+    model = joblib.load(".\ML\BalancedBaggin.joblib")
+    print(model)
+    preds = model.predict_proba([[29.85 ,37.85 ,1359.00 ,44.20 ,115.00 ,0.00]])
+    return preds
