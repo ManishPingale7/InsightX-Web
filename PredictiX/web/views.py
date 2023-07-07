4df0d25e723cbabe0d4db7c0366d0b9c45eaafc4
Manish Pingale <manishpingale2005@gmail.com>
2023-07-07 16:07:39 +0530

    Libraries fixed

--- PredictiX/web/views.py
+++ PredictiX/web/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import joblib
+import warnings
+warnings.filterwarnings("once")
 
 # Create your views here.
 
+
 def home(request):
-    print(makePrediction())
     return HttpResponse(makePrediction())
 
 
 def makePrediction():
     model = joblib.load(".\ML\BalancedBaggin.joblib")
-    print(model)
-    preds = model.predict_proba([[29.85 ,37.85 ,1359.00 ,44.20 ,115.00 ,0.00]])
+    preds = model.predict_proba([[29, 37, 1359, 44, 115, 0]])
     return preds
